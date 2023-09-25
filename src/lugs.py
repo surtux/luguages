4a24322b4e683e1257a932fc189f6b204ea5bc1d
surtux <surtux@users.noreply.github.com>
2023-09-25 01:57:42 +0100

    Update lugs.py

--- src/lugs.py
+++ src/lugs.py
@@ -1,1 +1,7 @@
-
+#The employee that validate tickets, will get the ticket id to update the document of the client
+#in the mongodb database with all his luguage
+#it will make it by using an ui to process this step very quickly
+#From the ui, the employee will select icon corresponding to the type of customer's luguage
+#He'll make sure to enter the correct quantity of each luguage, then generate through a machine
+#The number of tickets corresponding to the client's luguage. All these tickets will be stick on 
+#The client luguage
